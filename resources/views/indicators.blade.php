cfc90da3a71e9781353214268b65c045f8658ea0
emilynevitt <s4sdwork@MacBook-Pro.local>
2019-09-04 12:07:48 +0100

    front-end progress

---
+++ resources/views/indicators.blade.php
@@ -0,0 +1,241 @@
+@extends('layouts.app')
+
+@section('content')
+<!--
+<img src="{{ asset('img/odk-builder.jpg') }}" style="width: 100vw"/>
+-->
+
+<section class="hero-section set-bg" data-setbg="{{ asset('img/agroecology.jpg') }}" style="height: 110px !important">
+
+</section>
+
+
+<div class="container">
+    <div class="row mx-1 mb-5">
+        <h2>Indicators List</h2>
+
+        <p>Some sort of introduction to the list of indicators and what you can do from here?</p>
+        <p>Lorem ipsum pellentesque curabitur torquent est tincidunt est semper mauris fringilla viverra, sollicitudin
+            ipsum ullamcorper pharetra condimentum risus dapibus eget taciti fringilla, volutpat enim tempor curae justo
+            dapibus odio justo cras odio morbi quisque tempor mattis nostra cubilia facilisis mollis ullamcorper etiam
+            in vehicula, pulvinar lacus erat libero litora nisi feugiat aliquam metus erat.</p>
+
+    </div>
+
+
+<!--progress bar to be made -->
+
+
+<!--accordion-->
+    <div class="accordion" id="indicators">
+        <div class="accordion-group">
+</div>
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <button class="accordion-toggle btn tool-list tl-heading my-0" data-toggle="collapse" data-target="#cat1" href="#">
+                    Category #1
+                </button>
+            </div>
+            <div id="cat1" class="collapse">
+                <div class="accordion-inner">
+                    <div class="w-100">
+
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                                    Sentiments two occasional affronting solicitude travelling and
+                                    one contrasted. Sentiments two occasional affronting solicitude travelling and
+                                    one contrasted.Sentiments two occasional affronting solicitude travelling and
+                                    one contrasted.
+                                </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            Fortune day out married parties.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            Happiness remainder joy but earnestly for off.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            Took sold add play may none him few.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            If as increasing contrasted entreaties be.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            Now summer who day looked our behind moment coming.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                        <button class="btn tool-list tl-item">
+                            <div class="row">
+                                <div class="col-sm-10">
+                            Pain son rose more park way that.
+                        </div>
+                                <div class="col-2 d-none d-sm-block my-auto" style="text-align: right;">
+                                    + 
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+</div>
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <button class="accordion-toggle btn tool-list tl-heading my-0" data-toggle="collapse" data-target="#cat2" href="#">
+                    Category #2
+                </button>
+            </div>
+            <div id="cat2" class="collapse">
+                <div class="accordion-inner">
+                    <div class="w-100">
+
+                        <div class="tool-list tl-item">
+                            Sentiments two occasional affronting solicitude travelling and
+                            one contrasted.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Fortune day out married parties.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Happiness remainder joy but earnestly for off.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Took sold add play may none him few.
+                        </div>
+                        <div class="tool-list tl-item">
+                            If as increasing contrasted entreaties be.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Now summer who day looked our behind moment coming.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Pain son rose more park way that.
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <button class="accordion-toggle btn tool-list tl-heading my-0" data-toggle="collapse" data-target="#cat3" href="#">
+                    Category #3
+                </button>
+            </div>
+            <div id="cat3" class="collapse">
+                <div class="accordion-inner">
+                    <div class="w-100">
+
+                        <div class="tool-list tl-item">
+                            Sentiments two occasional affronting solicitude travelling and
+                            one contrasted.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Fortune day out married parties.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Happiness remainder joy but earnestly for off.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Took sold add play may none him few.
+                        </div>
+                        <div class="tool-list tl-item">
+                            If as increasing contrasted entreaties be.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Now summer who day looked our behind moment coming.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Pain son rose more park way that.
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <button class="accordion-toggle btn tool-list tl-heading my-0" data-toggle="collapse" data-target="#cat4" href="#">
+                    Category #4
+                </button>
+            </div>
+            <div id="cat4" class="collapse">
+                <div class="accordion-inner">
+                    <div class="w-100">
+
+                        <div class="tool-list tl-item">
+                            Sentiments two occasional affronting solicitude travelling and
+                            one contrasted.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Fortune day out married parties.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Happiness remainder joy but earnestly for off.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Took sold add play may none him few.
+                        </div>
+                        <div class="tool-list tl-item">
+                            If as increasing contrasted entreaties be.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Now summer who day looked our behind moment coming.
+                        </div>
+                        <div class="tool-list tl-item">
+                            Pain son rose more park way that.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- End Accordion -->
+
+   
+    </div>
+
+
+
+</div>
+
+
+
+
+@endsection
