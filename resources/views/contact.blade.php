ce610474aca24dbe7d7b7a50fea9c822a489ec8f
David Mills <d.e.mills@stats4sd.org>
2019-11-25 15:02:29 +0000

    adding recaptcha script

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -24,6 +24,7 @@ Contact
             @if(config('services.recaptcha.key'))
                 <div class="form-group {{ $errors->has('g-recaptcha-response') ? 'has-error' : '' }}">
                     <div class="g-recaptcha" data-sitekey="{{ config('services.recaptcha.key') }}"></div>
+                    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
                     <span class="text-danger">{{ $errors->first('g-recaptcha-response') }}</span>
                 </div>
             @endif
